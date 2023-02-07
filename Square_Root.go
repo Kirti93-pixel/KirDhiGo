4d77b9a31fbb0558e813f389cb8136e114a796c7
Kirti93-pixel <49kdhiman@gmail.com>
2023-02-07 12:27:39 +0530

    square root

---
+++ Square_Root.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+)
+
+func squareRootLinearSearch(x int) int {
+	if x == 0 || x == 1 {
+		return x
+	}
+	i := 1
+	for ; i <= x/i; i++ {
+		if x/i == i {
+			return i
+		}
+	}
+	return i - 1
+}
+
+func squareRootBinarySearch(x int) int {
+	if x == 0 || x == 1 {
+		return x
+	}
+	low, end, ans := 1, x, 0
+
+	for low <= end {
+		mid := int((low + end) / 2)
+		if mid*mid == x {
+			return mid
+		}
+
+		if mid*mid < x {
+			low = mid + 1
+			ans = mid // Just to get the lower bound Value (if we need upper bound value then we can add this in else condition)
+		} else {
+			end = mid - 1
+		}
+	}
+	return ans
+}
+
+func main() {
+	fmt.Println("squareRootLinearSearch for 16 is:::", squareRootLinearSearch(16))
+	fmt.Println("squareRootBinarySearch for 80 is:::", squareRootBinarySearch(80))
+}
