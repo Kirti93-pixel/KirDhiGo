49a4b215159549cb563058d0237533f3a11cda30
Kirti93-pixel <49kdhiman@gmail.com>
2023-02-07 15:37:04 +0530

    climb stairs

--- Climb_Stairs.go
+++ Climb_Stairs.go
@@ -7,16 +7,16 @@ func climbStairs(n int) int {
 	if n <= 2 {
 		return n
 	}
-	ways, twoBelowCurrentStep, oneBelowCurrentStep := 0, 1, 2
+	ways, first, second := 0, 1, 2
 	for i := 2; i < n; i++ {
-		ways = twoBelowCurrentStep + oneBelowCurrentStep
-		twoBelowCurrentStep, oneBelowCurrentStep = oneBelowCurrentStep, ways
+		ways = first + second
+		first, second = second, ways
 	}
 	return ways
 }
 
 func main() {
-	fmt.Println("Number of ways to climb 8th stair::", climbStairs(8))
-	fmt.Println("Number of ways to climb 8th stair::", climbStairs(3))
-	fmt.Println("Number of ways to climb 8th stair::", climbStairs(18))
+	fmt.Println("Number of ways to climb 5th stair::", climbStairs(5))
+	fmt.Println("Number of ways to climb 3rd stair::", climbStairs(3))
+	fmt.Println("Number of ways to climb 18th stair::", climbStairs(18))
 }
